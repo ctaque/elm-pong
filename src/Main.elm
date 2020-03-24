1ff152dee795bdd9da83ea8b06a216bf4a088b39
Cyprien Taque <cyprien.taque@gmail.com>
2020-03-24 18:47:56 +0100

    refactor coordinates

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Time
 
 -- MAIN
 
+
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
@@ -21,23 +22,42 @@ main =
 pxByMove : Int
 pxByMove =
     3
-barMoveIncrement: Int
-barMoveIncrement = 40
+
+
+barMoveIncrement : Int
+barMoveIncrement =
+    40
+
+
+barYOffset : Int
+barYOffset =
+    10
+
+
+barHeight : Int
+barHeight =
+    20
+
 
 type alias WindowSize =
     { width : Int
     , height : Int
     }
 
+type alias SetYPositionReturnType =
+    { y : Int
+    , direction : Int
+    , gameLost : Bool
+    }
 
 type alias Model =
-    { x : Int
-    , y : Int
+    { coordinates : (Int, Int)
     , xDirection : Int
     , yDirection : Int
     , windowSize : WindowSize
-    , barWidth : Int 
+    , barWidth : Int
     , barXOffset : Int
+    , gameLost : Bool
     }
 
 
@@ -46,16 +66,19 @@ type alias Flags =
     , windowWidth : Int
     }
 
+
 getBarWidth : Flags -> Int
-getBarWidth flags = 
+getBarWidth flags =
     Basics.floor (Basics.min (Basics.toFloat flags.windowWidth / 2) 300)
+
+
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { x = Basics.floor (toFloat flags.windowWidth / 2)
-      , y = Basics.floor (toFloat flags.windowHeight / 2)
+    ( { coordinates = (Basics.floor (toFloat flags.windowWidth / 2), Basics.floor (toFloat flags.windowHeight / 2))
       , xDirection = 1
       , yDirection = 1
-      , barXOffset = Basics.floor ((toFloat flags.windowWidth / 2) - ( Basics.toFloat (getBarWidth flags) / 2))
+      , barXOffset = Basics.floor ((toFloat flags.windowWidth / 2) - (Basics.toFloat (getBarWidth flags) / 2))
+      , gameLost = False
       , barWidth = getBarWidth flags
       , windowSize =
             { width = flags.windowWidth
@@ -90,42 +113,47 @@ setXPosition x windowSize direction =
         ( x - pxByMove, -1 )
 
 
-setYPosition : Int -> WindowSize -> Int -> ( Int, Int )
+setYPosition : Int -> WindowSize -> Int -> SetYPositionReturnType
 setYPosition y windowSize direction =
     if y <= 0 then
-        ( pxByMove, 1 )
+        { y = pxByMove, direction = 1, gameLost = False}
 
     else if y >= windowSize.height then
-        ( windowSize.height - pxByMove, -1 )
+        { y = windowSize.height - pxByMove, direction = -1, gameLost = True}
 
     else if direction == 1 then
-        ( y + pxByMove, 1 )
+        {  y = y + pxByMove, direction =  1, gameLost = False}
 
     else
-        ( y - pxByMove, -1 )
+        { y = y - pxByMove, direction = -1, gameLost = False }
+
 
 barOffsetFromLeft : Int -> Int
-barOffsetFromLeft currentPosition = 
+barOffsetFromLeft currentPosition =
     Basics.max currentPosition 0
-barOffsetFromRight: Int -> WindowSize -> Int -> Int
+
+
+barOffsetFromRight : Int -> WindowSize -> Int -> Int
 barOffsetFromRight nextPosition windowSize barWidth =
     Basics.min nextPosition (windowSize.width - barWidth)
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     let
         x =
-            setXPosition model.x model.windowSize model.xDirection
+            setXPosition (Tuple.first model.coordinates) model.windowSize model.xDirection
 
-        y =
-            setYPosition model.y model.windowSize model.yDirection
+        yResult =
+            setYPosition (Tuple.second model.coordinates) model.windowSize model.yDirection
     in
     case msg of
         Move _ ->
             ( { model
-                | x = Tuple.first x
-                , y = Tuple.first y
+            | coordinates = (Tuple.first x, yResult.y)
                 , xDirection = Tuple.second x
-                , yDirection = Tuple.second y
+                , yDirection = yResult.direction
+                , gameLost = yResult.gameLost
               }
             , Cmd.none
             )
@@ -137,18 +165,21 @@ update msg model =
             in
             case keyParsed of
                 "ArrowRight" ->
-                    ({ model
-                        |  barXOffset = barOffsetFromLeft ( barOffsetFromRight (model.barXOffset + barMoveIncrement) model.windowSize model.barWidth)
-                    }
-                    , Cmd.none)
+                    ( { model
+                        | barXOffset = barOffsetFromLeft (barOffsetFromRight (model.barXOffset + barMoveIncrement) model.windowSize model.barWidth)
+                      }
+                    , Cmd.none
+                    )
 
                 "ArrowLeft" ->
-                    ({ model
-                        | barXOffset = barOffsetFromLeft ( barOffsetFromRight (model.barXOffset - barMoveIncrement) model.windowSize model.barWidth)
-                    }
-                    , Cmd.none)
+                    ( { model
+                        | barXOffset = barOffsetFromLeft (barOffsetFromRight (model.barXOffset - barMoveIncrement) model.windowSize model.barWidth)
+                      }
+                    , Cmd.none
+                    )
+
                 _ ->
-                    (model, Cmd.none)
+                    ( model, Cmd.none )
 
 
 
@@ -176,17 +207,17 @@ view model =
         ]
         [ div
             [ style "position" "absolute"
-            , style "bottom" "10px"
+            , style "bottom" (String.fromInt barYOffset ++ "px")
             , style "transform" ("translateX(" ++ (String.fromInt model.barXOffset ++ "px)"))
             , style "background-color" "black"
-            , style "height" "20px"
+            , style "height" (String.fromInt barHeight ++ "px")
             , style "width" (String.fromInt model.barWidth ++ "px")
             ]
             []
         , div
             [ style "position" "absolute"
-            , style "top" (String.fromInt (.y model) ++ "px")
-            , style "left" (String.fromInt (.x model) ++ "px")
+            , style "top" (String.fromInt (Tuple.second model.coordinates) ++ "px")
+            , style "left" (String.fromInt (Tuple.first model.coordinates) ++ "px")
             , style "background-color" "blue"
             , style "border-radius" "100%"
             , style "width" "50px"
